b8e6838b98a76a997adce331cf781dc8d6091ed3
h4nabii <leon_xenos.xu@foxmail.com>
2023-11-02 22:52:36 +0800

    deploy page error, try fix

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,57 @@
-const {merge} = require("webpack-merge");
-const common = require("./webpack.common.js");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = merge(common, {
+module.exports = {
     mode: "production",
-
-});
+    entry: {
+        index: "./src/index.ts",
+    },
+    optimization: {
+        runtimeChunk: "single",
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.html$/,
+                loader: "html-loader",
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+};
